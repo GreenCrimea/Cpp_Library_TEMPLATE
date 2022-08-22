f771de6445c70bfc0ae3b7a891bf2ff3ed8b1211
greencrimea <tom.horton100@gmail.com>
2022-08-22 19:50:29 +1000

    .

---
+++ build/CMakeFiles/cpp_library_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_library_test.dir/main.cpp.o"
+  "CMakeFiles/cpp_library_test.dir/main.cpp.o.d"
+  "cpp_library_test"
+  "cpp_library_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_library_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gc/VS-Projects/Cpp_Library_test
+# Build directory: /home/gc/VS-Projects/Cpp_Library_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
